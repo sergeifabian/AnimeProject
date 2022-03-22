0d766136916bd223e6462a12adec2da177ab4bc6
Sergei Fabian <sf.dev@icloud.com>
2022-03-22 14:18:26 +0300

    Initial Commit

---
+++ AnimeProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimeProject
+//
+//  Created by Sergei Fabian on 22.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
