79ac0d9816bb51f21c40daf846315814591d45ef
Sergei Fabian <sf.dev@icloud.com>
2022-03-26 14:43:26 +0300

    Complete sign out logic

--- AnimeProject/Services/AuthService.swift
+++ AnimeProject/Services/AuthService.swift
@@ -9,10 +9,16 @@ import Foundation
 
 protocol AuthService {
     func signIn()
+    func signOut()
 }
 
 final class AuthServiceImpl: AuthService {
+    
     func signIn() {
         print("Sign in success")
+    }
+
+    func signOut() {
+        print("Sign out success")
     }
 }
--- AnimeProject/ViewController.swift
+++ AnimeProject/ViewController.swift
@@ -11,12 +11,26 @@ class ViewController: UIViewController {
 
     let authService: AuthService = AuthServiceImpl()
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    lazy var signIn: UIButton = {
+        let button = UIButton()
+        button.addTarget(self, action: #selector(signInAction), for: .touchUpInside)
+        button.setTitle("Sign in", for: .normal)
+        return button
+    }()
 
+    lazy var signOut: UIButton = {
+        let button = UIButton()
+        button.addTarget(self, action: #selector(signOutAction), for: .touchUpInside)
+        button.setTitle("Sign out", for: .normal)
+        return button
+    }()
+
+    @objc func signInAction() {
         authService.signIn()
     }
 
-
+    @objc func signOutAction() {
+        authService.signOut()
+    }
 }
 
